d6525fd6fd601c31f70e9558ebd6b14a49957e1b
Christopher Zerpa G. <christopherzerpagarcia@gmail.com>
2024-09-13 12:33:59 -0400

    Update Project Covid Data Exploration CZ.sql

--- Project Covid Data Exploration CZ.sql
+++ Project Covid Data Exploration CZ.sql
@@ -1,3 +1,5 @@
+--Hello There! This Project is to explore the Data about Covid 19 found in the page 'Our World in Data' To practice SQL and get some insights about it
+
 --Exploring the Dataset of Covid
 
 SELECT *
